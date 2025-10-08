5ad378a837a70cc891b8614f930259099bb4915e
PORTATILHP\nito9 <tamboleojuan2005@gmail.com>
2025-10-08 19:44:18 +0200

    Paso 6 Creado PostController@index para manejar la página de Blog y eliminado la función anónima en la ruta /blog. La ruta ahora llama al controlador, centralizando la lógica y permitiendo renderizado dinámico de los posts en la vista blog.blade.php.

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = [
+            ['title' => 'Post 1'],
+            ['title' => 'Post 2'],
+            ['title' => 'Post 3'],
+            ['title' => 'Post 4'],
+
+        ];
+        return view('blog', compact('posts'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,11 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('home');
 Route::view('contact', 'contact')->name('contact');
-    Route::get('blog', function(){
-        $posts = [
-            ['title' => 'Post 1'],
-            ['title' => 'Post 2'],
-            ['title' => 'Post 3'],
-            ['title' => 'Post 4'],
-
-        ];
-        return view('blog', compact('posts'));
-    })->name('blog');
+Route::get('blog' , [PostController::class, 'index'])->name('blog');
 Route::view('nosotros', 'about')->name('about');
 
 
