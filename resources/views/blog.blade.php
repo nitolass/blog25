f8b64130ecf0b052ab00ee23e59e8aecc1bbca13
PORTATILHP\nito9 <tamboleojuan2005@gmail.com>
2025-10-08 19:28:57 +0200

    Paso 5 Cambiada la ruta /blog de estática a dinámica: ahora usa Route::get con un array de posts y pasa los datos a la vista blog.blade.php, permitiendo mostrar contenido dinámico.

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,5 +1,8 @@
 <x-layout meta-title="Blog"
           meta-description="Descripción de la página de Blog">
     <h1>Blog</h1>
+    @foreach($posts as $post)
+        <h2>{{ $post['title'] }}</h2>
+    @endforeach
 </x-layout>
 
