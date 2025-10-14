173535fa599cb119d30705fe00e760826aa46664
PORTATILHP\nito9 <tamboleojuan2005@gmail.com>
2025-10-14 16:36:51 +0200

    Paso 12 Creamos posts desde nuestra web y guardamos el formulario en la base de datos

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -19,9 +19,15 @@ class PostController extends Controller
         return view('posts.create');
     }
 
-    public function store()
+    public function store(Request $request)
     {
-        return 'Guardando...';
+        $post = new Post();
+        $post-> title = $request->title;
+        $post -> body = $request->body;
+        $post->save();
+
+       // return redirect()->route('posts.index'); abajo de forma abreviada
+        return to_route('posts.index');
     }
 
     public function show(Post $post)
--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ Route::view('/', 'welcome')->name('home');
 Route::view('contact', 'contact')->name('contact');
 Route::get('blog', [PostController::class, 'index'])->name('posts.index');
 Route::get('blog/create', [PostController::class, 'create'])->name('posts.create');
-Route::get('blog', [PostController::class, 'store'])->name('posts.store');
+Route::post('blog', [PostController::class, 'store'])->name('posts.store');
 Route::get('blog/{post}', [PostController::class, 'show'])->name('posts.show');
 Route::view('nosotros', 'about')->name('about');
 
