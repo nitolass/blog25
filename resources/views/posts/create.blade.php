c544730e476791e059cbf7d36f3287d7c40a8874
PORTATILHP\nito9 <tamboleojuan2005@gmail.com>
2025-10-15 16:14:46 +0200

    Paso 18 refactorizamos codigo creando un nuevo archivo llamado form_fields.blade.php y lo añadimos en los otros con un @include

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,27 +5,8 @@
 
     <form method="POST" action="{{ route('posts.store') }}">
         @csrf
-        <label>
-            {{ __('Title') }} <br />
-            <input type="text" name="title" value="{{ old('title') }}">
+        @include('posts.form_fields')
 
-            @error('title')
-            <br/>
-            <small style="color: red">{{$message}}</small>
-            @enderror
-
-        </label>
-        <br />
-        <label>
-            {{ __('Body') }} <br />
-            <textarea name="body">{{ old('body') }}</textarea>
-
-            @error('body')
-            <br/>
-            <small style="color: red">{{$message}}</small>
-            @enderror
-
-        </label>
         <br />
         <button type="submit">{{ __('Send') }}</button>
         <br />
