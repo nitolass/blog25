6e2a65b50e00d92088bfb707f93d79f387aec34d
PORTATILHP\nito9 <tamboleojuan2005@gmail.com>
2025-10-17 19:31:33 +0200

    Paso 21 Cambiar la barra de navegacion y hemos metido vite y le hemos dado un poco de estilo a las rutas

--- resources/views/components/partials/navigation.blade.php
+++ resources/views/components/partials/navigation.blade.php
@@ -1,6 +1,6 @@
 <ul>
-    <li><a href="{{ route('home') }}">{{ __('Home') }}</a></li>
-    <li><a href="{{route('posts.index')}}">Blog</a></li>
-    <li><a href="{{ route('about') }}">{{ __('Nosotros') }}</a></li>
-    <li><a href="{{ route('contact') }}">{{ __('Contacto') }}</a></li>
+    <li><a href="{{ route('home') }}" class="{{ request()->routeIs('home') ? 'text-green-400' : 'text-gray-700' }}">{{ __('Home') }}</a></li>
+    <li><a href="{{route('posts.index')}}" class="{{ request()->routeIs('posts.index') ? 'text-green-400' : 'text-gray-700' }}">Blog</a></li>
+    <li><a href="{{ route('about') }}" class="{{ request()->routeIs('about') ? 'text-green-400' : 'text-gray-700' }}">{{ __('Nosotros') }}</a></li>
+    <li><a href="{{ route('contact') }}" class="{{ request()->routeIs('contact') ? 'text-green-400' : 'text-gray-700' }}">{{ __('Contacto') }}</a></li>
 </ul>
