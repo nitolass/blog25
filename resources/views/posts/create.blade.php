a29a1bb526712faaf070d2347ad4b74e3f3a1f0a
PORTATILHP\nito9 <tamboleojuan2005@gmail.com>
2025-10-22 15:24:39 +0200

    Paso 25

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,16 +1,28 @@
-<x-layout meta-title="Create new post" meta-description="Form to create a new Post">
-    <h1>{{__("Create new Post")}}</h1>
+
+    <x-app-layout meta-title="Create new post" meta-description="Form to create a new Post">
+        <x-slot name="header">
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                {{ __('Create new post') }}
+            </h2>
+        </x-slot>
 
 
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6 text-gray-900 dark:text-gray-100">
+                <form method="POST" action="{{ route('posts.store') }}">
+                    @csrf
+                    @include('posts.form_fields')
+                    <br />
+                    <button type="submit">{{ __('Send') }}</button>
+                    <br />
+                </form>
+            </div>
+        </div>
+    </div>
 
-    <form method="POST" action="{{ route('posts.store') }}">
-        @csrf
-        @include('posts.form_fields')
+</div>
 
-        <br />
-        <button type="submit">{{ __('Send') }}</button>
-        <br />
-    </form>
-    <a href="{{ route('posts.index') }}">{{ __('Back') }}</a>
-</x-layout>
-
+<a href="{{ route('posts.index') }}">{{ __('Back') }}</a>
+</x-app-layout>
